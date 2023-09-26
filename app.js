fbf0e1ded1f937f6132e3781eac9c021b0dd9047
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-09-26 13:00:32 +0300

    end the app

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ let quoteText = document.querySelector("#text");
 let quoteAuthor = document.querySelector("#author");
 let newQuoteBtn = document.querySelector("#new-quote");
 let tweetBtn = document.querySelector("#tweet-quote");
+
+newQuoteBtn.addEventListener("click", function getQuote() {
+  fetch("https://api.quotable.io/quotes/random?maxLength=50")
+    .then((res) => res.json())
+    .then((data) => {
+      quoteText.innerHTML = `"${data[0].content}"`;
+      quoteAuthor.innerHTML = `- ${data[0].author}`;
+    })
+    .catch((err) => console.log(err));
+});
+
+tweetBtn.addEventListener("click", function tweetQuote() {
+  let quote = quoteText.innerHTML;
+  let author = quoteAuthor.innerHTML;
+  let twitterUrl = `https://twitter.com/intent/tweet?text=${quote} ${author}`;
+  window.open(twitterUrl, "_blank");
+});

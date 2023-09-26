16adc5c93565bacacd53f6302449b464e2160464
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-09-26 12:38:22 +0300

    end styling

---
+++ app.js
@@ -0,0 +1,4 @@
+let quoteText = document.querySelector("#text");
+let quoteAuthor = document.querySelector("#author");
+let newQuoteBtn = document.querySelector("#new-quote");
+let tweetBtn = document.querySelector("#tweet-quote");
